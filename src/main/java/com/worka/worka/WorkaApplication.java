941b648033b2efa3ebb0d16f7ed4e995a2d0a585
hstla <ree7243@gmail.com>
2024-11-20 20:41:54 +0900

    프로젝트 세팅

---
+++ src/main/java/com/worka/worka/WorkaApplication.java
@@ -0,0 +1,13 @@
+package com.worka.worka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worka/worka/WorkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worka.worka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
